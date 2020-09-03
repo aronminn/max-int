fdaa6deb74f43a9025c19da4c52373ee65b3c41e
Aron Bergur <aronnemi@gmail.com>
2020-09-03 09:33:47 +0000

    Algorithm written for max_int

---
+++ max_int.py
@@ -0,0 +1,10 @@
+
+num_int = int(input("Input a number: "))    # Do not change this line
+# Fill in the missing code
+
+# Ef num int er stærri heldur en seinasta tala þá geymum við hana.
+# Þegar num int er ekki negative þá hættum við að spurja um input og prentum stærstu töluna.
+
+        
+
+print("The maximum is", max_int)    # Do not change this line
