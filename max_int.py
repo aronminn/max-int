91763c13894d1297a2b97bb13de92b0980539a10
Aron Bergur <aronnemi@gmail.com>
2020-09-03 09:37:09 +0000

    Program created

--- max_int.py
+++ max_int.py
@@ -4,7 +4,11 @@ num_int = int(input("Input a number: "))    # Do not change this line
 
 # Ef num int er stærri heldur en seinasta tala þá geymum við hana.
 # Þegar num int er ekki negative þá hættum við að spurja um input og prentum stærstu töluna.
+max_int = 0
+while num_int > 0:
+    if num_int > max_int:
+        max_int = num_int
 
-        
+    num_int = int(input("Input a number: "))
 
 print("The maximum is", max_int)    # Do not change this line
