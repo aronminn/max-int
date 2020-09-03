42ed6aa2745401a8412fe6fecc8b4c9f5f0ec3ac
Aron Bergur <aronnemi@gmail.com>
2020-09-03 10:19:14 +0000

    changes made

--- sequence.py
+++ sequence.py
@@ -13,9 +13,6 @@ third_number = 0
 next_number = 0
 for num in range(n):
     
-    #fyrsta talan er þá = first_number(1) + second_number(0) + third_number(0) = 1
-
-    #then the first number has to be = next number and so on
     next_number = first_number + second_number + third_number
 
     third_number = second_number
